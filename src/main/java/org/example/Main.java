99156488bd1bdecb346ced8e86937d6d2ea85b97
jrzabott <jrzabott@gmail.com>
2025-08-17 21:53:54 +0100

    Adding benchmarks and functionalities for test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    private static final Map<YearMonth, StartEndTimePair> MILLIS_MAP = new HashMap<>();
+    static {
+        initMillisMap();
+    }
+
+    private static void initMillisMap() {
+        for (int i = 1970; i < 2100; i++) {
+            for (int j = 1; j <= 12; j++) {
+                YearMonth yearMonth = YearMonth.of(i, j);
+                long startTime = yearMonth.minusMonths(1).atEndOfMonth().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+                long endTime = yearMonth.atEndOfMonth().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+                MILLIS_MAP.put(yearMonth, new StartEndTimePair(startTime, endTime));
+            }
+        }
+    }
+
+    public static Result createAnObjViaCalendar(int aNumber, int aYear, int aMonth) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+
+        cal.set(Calendar.YEAR, aYear);
+        cal.set(Calendar.MONTH, aMonth);
+
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        long startTime = cal.getTimeInMillis();
+        cal.add(Calendar.DAY_OF_MONTH, 1);
+        long endTime = cal.getTimeInMillis();
+
+        return new Result(aNumber, startTime, endTime);
+    }
+
+    public static Result createAnObjViaNewJavaTimeAPI(int aNumber, int aYear, int aMonth) {
+        YearMonth yearMonth = YearMonth.of(aYear, aMonth);
+        long startTime = yearMonth.minusMonths(1).atEndOfMonth().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        long endTime = yearMonth.atEndOfMonth().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        return new Result(aNumber, startTime, endTime);
+    }
+
+    public static Result createAndObjUsingMemoization(int aNumber, int aYear, int aMonth) {
+        long startTime = MILLIS_MAP.get(YearMonth.of(aYear, aMonth)).startTime();
+        long endTime = MILLIS_MAP.get(YearMonth.of(aYear, aMonth)).endTime();
+        return new Result(aNumber, startTime, endTime);
+    }
+
+
+    public record Result(int number, long startTime, long endtime) {}
+    public record StartEndTimePair(long startTime, long endTime) {}
+}
---
+++ src/main/java/org/example/MainBenchmark.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.profile.JavaFlightRecorderProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@SuppressWarnings("unused")
+@BenchmarkMode(Mode.All)
+@State(Scope.Benchmark)
+@OutputTimeUnit(java.util.concurrent.TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, batchSize = 5_000)
+@Fork(value = 2)
+public class MainBenchmark {
+
+    int number;
+    int year;
+    int month;
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .addProfiler(JavaFlightRecorderProfiler.class)
+                .include(MainBenchmark.class.getName())
+                .build();
+        new Runner(options).run();
+    }
+
+    @Setup(Level.Invocation)
+    public void setup() {
+        number = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+        year = ThreadLocalRandom.current().nextInt(1970, 2100);
+        month = ThreadLocalRandom.current().nextInt(1, 13);
+    }
+
+    @Benchmark
+    public void benchmarkCreateAnObjViaCalendar(Blackhole blackhole) {
+        blackhole.consume(Main.createAnObjViaCalendar(number, year, month));
+    }
+
+    @Benchmark
+    public void benchmarkCreateAnObjViaNewJavaTimeAPI(Blackhole blackhole) {
+        blackhole.consume(Main.createAnObjViaNewJavaTimeAPI(number, year, month));
+    }
+
+    @Benchmark
+    public void benchmarkCreateAndObjUsingMemoization(Blackhole blackhole) {
+        blackhole.consume(Main.createAndObjUsingMemoization(number, year, month));
+    }
+}
